cca8d23bf12a994610bb4608b41b2da986a3ff59
Rodion <rodionspriridonov14@gmail.com>
2022-11-24 23:15:15 +0100

    project 2

--- script.js
+++ script.js
@@ -11,8 +11,28 @@ const personalMovieDB = {
     privat: false
 }
 
-const film = prompt('Один из последних просмотренных фильмов?'),
-    rating = prompt('На сколько оцените его?');
+if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман')
+} else {
+    alert('Прозошла ошибка')
+}
+let film = prompt('Один из последних просмотренных фильмов?');
+for(let i = 0; i < personalMovieDB.count; i++) {
+    while(!film || film.length > 50) {
+        film = prompt('Введите фильм который вы уже посмотрели!')
+    }
 
-personalMovieDB.movies[film] = rating;
+    const rating = prompt('На сколько оцените его?');
+    personalMovieDB.movies[film] = rating;
+    console.log(i)
+    if ((i + 1) != personalMovieDB.count) {
+        film = prompt('Какие еще фильмы вы смотрели?')
+    } else {
+        break
+    }
+}
 console.log(personalMovieDB)
